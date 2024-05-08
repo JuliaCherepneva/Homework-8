ddb6149566dd6f8faa054d52ab938fd22dc329cf
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-05-09 00:17:31 +0400

    Выполненное домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,6 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\nТренировка");
-        int[] weights = {58, 59, 60, 61, 59, 58, 57, 0, 0, 0, 0, 0};
-        weights[0] = 58;
-        int januaryWeight = weights[0];
-        System.out.println(weights[8]);
-        System.out.println(weights[9]);
-        System.out.println(januaryWeight);
-        int january = 0;
-        System.out.println(weights[january]);
-        System.out.println();
-        for (int i = 0; i < weights.length; i++) {
-            System.out.println(weights[i]);
-        }
-        System.out.println(weights.length);
-        System.out.println(Arrays.toString(weights));
-
         System.out.println("\nЗадача 1");
         {
             int[] massiv1 = new int[]{1, 2, 3};
@@ -88,4 +72,6 @@ public class Main {
         }
     }
 }
+
+
 
